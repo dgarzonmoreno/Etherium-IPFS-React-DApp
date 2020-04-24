9f90295290196dce67863690ba8e07a64e556759
dgarzonmoreno <dgarzonmoreno@ufl.edu>
2020-04-23 23:20:27 -0400

    All scripts added - frontend left TODO

---
+++ src/components/EtheriumApp.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const EtheriumApp = () => {
+
+    return(
+        <h1>HELLO BOI</h1>
+    );
+};
+
+export default EtheriumApp;
---
+++ src/storehash.js
@@ -0,0 +1,35 @@
+import web3 from './web3';
+
+const contractAddress = "0x07fa978177546920bb281b091fa5c1ae6587b4ef";
+const abi = [
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "x",
+				"type": "string"
+			}
+		],
+		"name": "sendHash",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "getHash",
+		"outputs": [
+			{
+				"name": "x",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
+
+export default new web3.eth.Contract(abi, contractAddress);
